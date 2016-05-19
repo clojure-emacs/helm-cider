590a2f0f21d097d52fbf101a613916a0a09a0124
Tianxiang Xiong <tianxiang.xiong@gmail.com>
2016-05-18 22:48:30 -0500

    Wrap preselections in symbol boundary delimiters.

--- helm-cider.el
+++ helm-cider.el
@@ -71,7 +71,7 @@ sources."
   '(("Search in namespace" . helm-cider-apropos-symbol)
     ("Search in namespace with docs" . helm-cider-apropos-symbol-doc)
     ("Find definition" . (lambda (ns)
-			   (cider-find-ns nil ns)))
+                           (cider-find-ns nil ns)))
     ("Set REPL namespace" . cider-repl-set-ns))
   "Actions for Helm CIDER apropos namespaces."
   :group 'helm-cider-apropos
@@ -80,7 +80,7 @@ sources."
 (defcustom helm-cider-apropos-actions
   '(("CiderDoc" . cider-doc-lookup)
     ("Find definition" . (lambda (candidate)
-			   (cider-find-var nil candidate)))
+                           (cider-find-var nil candidate)))
     ("Find on Grimoire" . cider-grimoire-lookup))
   "Actions for Helm CIDER apropos symbols."
   :group 'helm-cider-apropos
@@ -149,11 +149,11 @@ NS matches a string equal to itself, or a string ending in \"*\"
 that is a prefix of NS, excluding the \"*\"."
   (catch 'excluded
     (dolist (ex (or excluded-ns
-		    helm-cider-apropos-excluded-ns))
+                    helm-cider-apropos-excluded-ns))
       (when (or (and (string-suffix-p "*" ex)
-		     (string-prefix-p (substring ex 0 (1- (length ex))) ns))
-		(string= ns ex))
-	(throw 'excluded t)))))
+                     (string-prefix-p (substring ex 0 (1- (length ex))) ns))
+                (string= ns ex))
+        (throw 'excluded t)))))
 
 (defun helm-cider--apropos-dicts (&optional excluded-ns)
   "List of CIDER apropos results (nREPL dicts).
@@ -161,10 +161,10 @@ that is a prefix of NS, excluding the \"*\"."
 Symbols in namespaces in EXCLUDED-NS are excluded.  If not
 provided, `helm-cider-apropos-excluded-ns' is used."
   (cl-loop with excluded-ns = (or excluded-ns helm-cider-apropos-excluded-ns)
-	   for dict in (cider-sync-request:apropos "")
-	   unless (helm-cider--excluded-ns-p (helm-cider--symbol-ns (nrepl-dict-get dict "name"))
-					     excluded-ns)
-	   collect dict))
+           for dict in (cider-sync-request:apropos "")
+           unless (helm-cider--excluded-ns-p (helm-cider--symbol-ns (nrepl-dict-get dict "name"))
+                                             excluded-ns)
+           collect dict))
 
 (defun helm-cider--apropos-hashtable (dicts)
   "Build a hash table from CIDER apropos results (DICTS).
@@ -174,7 +174,7 @@ dict objects)."
   (let ((ht (make-hash-table :test 'equal)))
     (dolist (dict dicts)
       (let ((ns (helm-cider--symbol-ns (nrepl-dict-get dict "name"))))
-	(puthash ns (cons dict (gethash ns ht)) ht)))
+        (puthash ns (cons dict (gethash ns ht)) ht)))
     ht))
 
 (defun helm-cider--apropos-candidate (dict)
@@ -183,8 +183,8 @@ dict objects)."
 DICT is an nREPL dict."
   (nrepl-dbind-response dict (name type)
     (cons (propertize (helm-cider--symbol-name name)
-		      'face (helm-cider--symbol-face type))
-	  name)))
+                      'face (helm-cider--symbol-face type))
+          name)))
 
 (defun helm-cider--apropos-doc-candidate (dict)
   "Create a Helm CIDER apropos doc candidate.
@@ -197,8 +197,8 @@ DICT is an nREPL dict."
       (insert "\n")
       ;; Doc
       (let ((beg (point)))
-	(insert doc "\n")
-	(fill-region beg (point-max)))
+        (insert doc "\n")
+        (fill-region beg (point-max)))
       ;; Candidate
       (cons (buffer-string) name))))
 
@@ -215,14 +215,14 @@ If FOLLOW is true, use `helm-follow-mode' for source."
   (helm-build-sync-source ns
     :action helm-cider-apropos-actions
     :candidate-transformer (lambda (candidates)
-			     (seq-sort (lambda (a b)
-					 (string< (cdr a) (cdr b)))
-				       candidates))
+                             (seq-sort (lambda (a b)
+                                         (string< (cdr a) (cdr b)))
+                                       candidates))
     :candidates (let ((fn (if doc
-			      #'helm-cider--apropos-doc-candidate
-			    #'helm-cider--apropos-candidate)))
-		  (mapcar fn (or dicts
-				 (cider-sync-request:apropos "" ns))))
+                              #'helm-cider--apropos-doc-candidate
+                            #'helm-cider--apropos-candidate)))
+                  (mapcar fn (or dicts
+                                 (cider-sync-request:apropos "" ns))))
     :follow (when follow 1)
     :multiline doc
     :nomark t
@@ -238,11 +238,11 @@ EXCLUDED-NS are excluded.  If not provided,
 
 If DOC is true, include symbol documentation in candidates."
   (cl-loop with ht = (helm-cider--apropos-hashtable
-		      (helm-cider--apropos-dicts excluded-ns))
-	   for ns being the hash-keys in ht using (hash-value dicts)
-	   collect (helm-cider--apropos-source ns dicts doc helm-cider-apropos-follow)
-	   into sources
-	   finally (return (sort sources (helm-cider--make-sort-sources-fn)))))
+                      (helm-cider--apropos-dicts excluded-ns))
+           for ns being the hash-keys in ht using (hash-value dicts)
+           collect (helm-cider--apropos-source ns dicts doc helm-cider-apropos-follow)
+           into sources
+           finally (return (sort sources (helm-cider--make-sort-sources-fn)))))
 
 (defun helm-cider--apropos-ns-source (&optional excluded-ns)
   "Helm source of namespaces.
@@ -252,9 +252,9 @@ Namespaces in EXCLUDED-NS are excluded.  If not provided,
   (helm-build-sync-source "Clojure Namespaces"
     :action helm-cider-apropos-ns-actions
     :candidates (cl-loop for ns in (cider-sync-request:ns-list)
-			 unless (helm-cider--excluded-ns-p ns excluded-ns)
-			 collect (cider-propertize ns 'ns) into all
-			 finally (return (sort all #'string<)))
+                         unless (helm-cider--excluded-ns-p ns excluded-ns)
+                         collect (cider-propertize ns 'ns) into all
+                         finally (return (sort all #'string<)))
     :nomark t
     :volatile t))
 
@@ -267,41 +267,41 @@ copy of `helm-map'."
     ;; Select namespace
     (define-key keymap (kbd helm-cider-apropos-ns-key)
       (lambda ()
-	(interactive)
-	(helm-exit-and-execute-action (lambda (candidate)
-					(helm-cider-apropos-ns candidate)))))
+        (interactive)
+        (helm-exit-and-execute-action (lambda (candidate)
+                                        (helm-cider-apropos-ns candidate)))))
     ;; Apropos with docs
     (dolist (key (cl-mapcan (lambda (command)
-			      (where-is-internal command cider-mode-map))
-			    '(cider-apropos-documentation
-			      cider-apropos-documentation-select)))
+                              (where-is-internal command cider-mode-map))
+                            '(cider-apropos-documentation
+                              cider-apropos-documentation-select)))
       (define-key keymap key
-	(lambda ()
-	  (interactive)
-	  (helm-exit-and-execute-action
-	   (lambda (candidate)
-	     (with-helm-after-update-hook
-	       (with-helm-buffer
-		 (helm-preselect (helm-cider--symbol-name candidate)
-				 (helm-cider--symbol-ns candidate)))
-	       (recenter 1))
-	     (helm-cider-apropos-symbol-doc))))))
+        (lambda ()
+          (interactive)
+          (helm-exit-and-execute-action
+           (lambda (candidate)
+             (with-helm-after-update-hook
+               (with-helm-buffer
+                 (helm-preselect (helm-cider--symbol-name candidate)
+                                 (helm-cider--symbol-ns candidate)))
+               (recenter 1))
+             (helm-cider-apropos-symbol-doc))))))
     ;; Apropos
     (dolist (key (cl-mapcan (lambda (command)
-			      (where-is-internal command cider-mode-map))
-			    '(cider-apropos
-			      cider-apropos-select)))
+                              (where-is-internal command cider-mode-map))
+                            '(cider-apropos
+                              cider-apropos-select)))
       (define-key keymap key
-	(lambda ()
-	  (interactive)
-	  (helm-exit-and-execute-action
-	   (lambda (candidate)
-	     (with-helm-after-update-hook
-	       (with-helm-buffer
-		 (helm-preselect (helm-cider--symbol-name candidate)
-				 (helm-cider--symbol-ns candidate))
-		 (recenter 1)))
-	     (helm-cider-apropos-symbol))))))
+        (lambda ()
+          (interactive)
+          (helm-exit-and-execute-action
+           (lambda (candidate)
+             (with-helm-after-update-hook
+               (with-helm-buffer
+                 (helm-preselect (helm-cider--symbol-name candidate)
+                                 (helm-cider--symbol-ns candidate))
+                 (recenter 1)))
+             (helm-cider-apropos-symbol))))))
     keymap))
 
 
@@ -313,9 +313,9 @@ copy of `helm-map'."
   (let ((keymap (copy-keymap (or keymap helm-map))))
     (define-key keymap (kbd helm-cider-apropos-symbol-doc-key)
       (lambda ()
-	(interactive)
-	(helm-exit-and-execute-action (lambda (candidate)
-					(helm-cider-apropos-symbol-doc candidate)))))
+        (interactive)
+        (helm-exit-and-execute-action (lambda (candidate)
+                                        (helm-cider-apropos-symbol-doc candidate)))))
     keymap))
 
 
@@ -343,13 +343,13 @@ browsing documentation."
   (when source
     (with-helm-after-update-hook
       (with-helm-buffer
-	(helm-goto-source source)
-	(helm-next-line))))
+        (helm-goto-source source)
+        (helm-next-line))))
   (helm :buffer "*Helm Clojure Symbols*"
-	:candidate-number-limit 9999
-	:keymap (helm-cider--apropos-map)
-	:preselect (cider-symbol-at-point t)
-	:sources (helm-cider--apropos-sources nil doc)))
+        :candidate-number-limit 9999
+        :keymap (helm-cider--apropos-map)
+        :preselect (concat "\\_<" (cider-symbol-at-point t) "\\_>")
+        :sources (helm-cider--apropos-sources nil doc)))
 
 ;;;###autoload
 (defun helm-cider-apropos-symbol-doc (&optional source)
@@ -373,9 +373,9 @@ the default selection."
   (interactive)
   (cider-ensure-connected)
   (helm :buffer "*Helm Clojure Namespaces*"
-	:keymap (helm-cider--apropos-ns-map)
-	:preselect (helm-cider--symbol-ns (or ns-or-qualified-name ""))
-	:sources (helm-cider--apropos-ns-source)))
+        :keymap (helm-cider--apropos-ns-map)
+        :preselect (concat "\\_<" (helm-cider--symbol-ns (or ns-or-qualified-name "")) "\\_>")
+        :sources (helm-cider--apropos-ns-source)))
 
 ;;;###autoload
 (defun helm-cider-apropos (&optional arg)
@@ -388,8 +388,8 @@ If ARG is raw prefix argument \\[universal-argument]
 \\[universal-argument], choose namespace before symbol."
   (interactive "P")
   (cond ((equal arg '(16)) (helm-cider-apropos-ns))
-	((equal arg '(4)) (helm-cider-apropos-symbol-doc))
-	(t (helm-cider-apropos-symbol))))
+        ((equal arg '(4)) (helm-cider-apropos-symbol-doc))
+        (t (helm-cider-apropos-symbol))))
 
 
 ;;;; Key bindings
